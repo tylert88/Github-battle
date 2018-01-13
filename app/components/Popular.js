7119074fbfeae6ed84ac5f79deb76c24b0267560
Tyler Torres <tyler.john.torres@gmail.com>
2018-01-12 22:02:19 -0700

    added github api fetch util & completed the render popularGrid component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -19,55 +19,28 @@ function SelectLanguage(props) {
 	)
 }
 
-// function RepoGrid(props) {
-// 	return (
-// 		<ul className="popular-list">
-// 			{props.repos.map(repo, index => (
-// 				<li key="repo.name" className="popular-item">
-// 					<div className="popular-rank">#{index + 1}</div>
-// 					<ul className="space-list-items">
-// 						<li>
-// 							<img
-// 								className="avatar"
-// 								src={repo.owner.avatar_url}
-// 								alt={'Avatar for ' + repo.owner.login}
-// 							/>
-// 						</li>
-// 						<li>
-// 							<a href={repo.html_url}>{repo.name}</a>
-// 						</li>
-// 						<li>@{repo.owner.login}</li>
-// 						<li>{repo.stargazers_count} stars</li>
-// 					</ul>
-// 				</li>
-// 			))}
-// 		</ul>
-// 	)
-// }
 function RepoGrid(props) {
 	return (
 		<ul className="popular-list">
-			{props.repos.map(function(repo, index) {
-				return (
-					<li key={repo.name} className="popular-item">
-						<div className="popular-rank">#{index + 1}</div>
-						<ul className="space-list-items">
-							<li>
-								<img
-									className="avatar"
-									src={repo.owner.avatar_url}
-									alt={'Avatar for ' + repo.owner.login}
-								/>
-							</li>
-							<li>
-								<a href={repo.html_url}>{repo.name}</a>
-							</li>
-							<li>@{repo.owner.login}</li>
-							<li>{repo.stargazers_count} stars</li>
-						</ul>
-					</li>
-				)
-			})}
+			{props.repos.map((repo, index) => (
+				<li key="repo.name" className="popular-item">
+					<div className="popular-rank">#{index + 1}</div>
+					<ul className="space-list-items">
+						<li>
+							<img
+								className="avatar"
+								src={repo.owner.avatar_url}
+								alt={'Avatar for ' + repo.owner.login}
+							/>
+						</li>
+						<li>
+							<a href={repo.html_url}>{repo.name}</a>
+						</li>
+						<li>@{repo.owner.login}</li>
+						<li>{repo.stargazers_count} stars</li>
+					</ul>
+				</li>
+			))}
 		</ul>
 	)
 }
